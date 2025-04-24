9753d9b5b50841825cb146d4902107d75b58634e
German Bejarano Cardenas <gbejarano1@unm.edu>
2025-04-24 12:14:51 -0600

    testing bug fixing

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,7 @@ import Brillo
 main :: IO()
 
 main = display
-  (InWindow "Nice Window finally" (800, 600) (10, 10))
+  (InWindow "Window Title" (800, 600) (10, 10))
   yellow
   (Circle 125)
 
--- main :: IO ()
--- main = putStrLn "Hello, Haskell!"
