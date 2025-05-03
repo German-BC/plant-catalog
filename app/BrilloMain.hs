74b310463c572914f948f4c8a64898f76646d7f3
German Bejarano Cardenas <gbejarano1@unm.edu>
2025-05-03 12:12:57 -0600

    paging system WIP

--- app/BrilloMain.hs
+++ app/BrilloMain.hs
@@ -1,18 +1,17 @@
 module BrilloMain (run) where 
 
 import Brillo 
-import Brillo.Data.Color
-import Brillo.Data.Display
-import Brillo.Data.Picture
-import Brillo.Interface.IO.Interact
+import Brillo.Interface.IO.Game
 
-import Plant 
+import Plant
+  ( Plant(Plant), bloomTime, commonName, botanicalName, sunNeed, waterNeed,
+    testPlantsLg ) 
 
 -- Converting a Plant to Picture
 plantToPicture :: Float -> Int -> Int -> Plant -> Picture 
 plantToPicture yOffset selected index plant = 
     let textColor = if index == selected then magenta else darkGreen 
-    in translate (-200) (yOffset - fromIntegral index * 75) $
+    in translate (-200) (yOffset - fromIntegral index * 40) $
     color textColor $
     scale 0.15 0.15 $ 
     text (commonName plant)
@@ -25,7 +24,7 @@ plantsToPicture selected plants =
 -- Draw selected plant details
 detailsToPicture :: Plant -> Picture 
 detailsToPicture plant = 
-    translate 10 200 $ 
+    translate 10 180 $ 
     scale 0.15 0.15 $ 
     color brownish $ 
     pictures 
@@ -38,47 +37,119 @@ detailsToPicture plant =
         ]
 
 -- Main draw function
-draw :: (Int, [Plant]) -> Picture 
-draw (selected, plants) = 
-    pictures 
-    [
-        translate (-150) 0 (plantsToPicture selected plants),
-        detailsToPicture (plants !! selected)
-    ]
+-- draw :: (Int, [Plant]) -> Picture 
+-- draw (selected, plants) = 
+--     pictures 
+--     [
+--         -- Section backgrounds
+--         translate (-200) 0 $ color (makeColor 0.87 0.78 0.64 0.5) $ rectangleSolid 390 530,
+--         translate 200 0 $ color (makeColor 0.87 0.78 0.64 0.5) $ rectangleSolid 390 530,
+--         -- List entries and detail view
+--         translate (-190) (-40) (plantsToPicture selected plants),
+--         detailsToPicture (plants !! selected)
+--     ]
+
+-- Draw UIState
+draw :: UIState -> Picture 
+draw (UIState page sel plants) =
+    let visiblePlants = plantsOnPage page plants 
+        selectedPlant = if null visiblePlants 
+                        then Plant "No selection" "" "" "" ""
+                        else visiblePlants !! sel 
+    in pictures 
+        [
+            -- Section backgrounds
+            translate (-200) 0 $ color (makeColor 0.87 0.78 0.64 0.5) $ rectangleSolid 390 530,
+            translate 200 0 $ color (makeColor 0.87 0.78 0.64 0.5) $ rectangleSolid 390 530,
+
+            -- plant list 
+            translate (-190) (-40) (plantsToPicture sel visiblePlants),
+
+            -- Detail view of selection
+            detailsToPicture selectedPlant 
+        ]                    
 
 -- Key input event handler
-handleInput :: Event -> (Int, [Plant]) -> (Int, [Plant])
-handleInput (EventKey (SpecialKey KeyUp) Down _ _) (sel, ps) =
-    (max 0 (sel - 1), ps)
-handleInput (EventKey (SpecialKey KeyDown) Down _ _) (sel, ps) =
-    (min (length ps - 1) (sel + 1), ps)
-handleInput _ state = state 
+--handleInput :: Event -> (Int, [Plant]) -> (Int, [Plant])
+-- handleInput (EventKey (SpecialKey KeyUp) Down _ _) (sel, ps) =
+--     (max 0 (sel - 1), ps)
+-- handleInput (EventKey (SpecialKey KeyDown) Down _ _) (sel, ps) =
+--     (min (length ps - 1) (sel + 1), ps)
+-- handleInput _ state = state 
+handleInput :: Event -> UIState -> IO UIState
+handleInput (EventKey (SpecialKey KeyUp) Down _ _) (UIState page sel plants) =
+    return $ UIState page (max 0 (sel - 1)) plants
+
+handleInput (EventKey (SpecialKey KeyDown) Down _ _) (UIState page sel plants) =
+    let maxIndex = min (pageSize - 1) (length (plantsOnPage page plants) - 1)
+    in return $ UIState page (min maxIndex (sel + 1)) plants
+
+handleInput (EventKey (SpecialKey KeyPageUp) Down _ _) (UIState page sel plants) =
+    let newPage = max 0 (page - 1) 
+        maxSel = length (plantsOnPage newPage plants) - 1 
+    in return $ UIState newPage (min sel maxSel) plants 
+
+handleInput (EventKey (SpecialKey KeyPageDown) Down _ _) (UIState page sel plants) =
+    let totalPages = (length plants - 1) `div` pageSize
+        newPage = min totalPages (page + 1)
+        maxSel = length (plantsOnPage newPage plants) - 1 
+    in return $ UIState newPage (min sel maxSel) plants
+
+handleInput _ state = return state
 
 -- Animation step 
-step :: Float -> (Int, [Plant]) -> (Int, [Plant])
-step _ state = state 
+--step :: Float -> (Int, [Plant]) -> (Int, [Plant])
+step :: Float -> UIState -> IO UIState
+step _ state = return state 
 
 -- -- Current state 
 -- draw :: (Int, [Plant]) -> Picture 
 -- draw (selected, plants) = plantsToPicture selected plants 
 
+-- App state record. Keep track of current page, the currently selected item,
+-- and the curernt list of plants
+data UIState = UIState
+    {
+        currentPage :: Int,
+        selectedIndex :: Int, 
+        plantList :: [Plant]
+    }
+
+-- Entries per page constant
+pageSize :: Int 
+pageSize = 10
+
+-- Page entries subset
+plantsOnPage :: Int -> [Plant] -> [Plant]
+plantsOnPage page plants = take pageSize $ drop (page * pageSize) plants
+
 -- Color definitions
 beige :: Color
-beige = makeColor 0.93 0.87 0.71 1.0
+beige = makeColor 0.99 0.95 0.86 1.0
 
 darkGreen :: Color 
-darkGreen = makeColor 0.325 0.439 0.086 1.0
+darkGreen = makeColor 0.33 0.45 0.09 1.0
 
 brownish :: Color 
-brownish = makeColor 0.388 0.29 0.043 1.0
+brownish = makeColor 0.39 0.30 0.05 1.0
 
 -- 
+-- run :: IO ()
+-- run = play 
+--     (InWindow "Native Plant Catalog" (800, 600) (10, 10))
+--     beige 
+--     30
+--     UIState 0 0 testPlantsLg
+--     draw
+--     handleInput
+--     step
+
 run :: IO ()
-run = play 
+run = playIO
     (InWindow "Native Plant Catalog" (800, 600) (10, 10))
     beige 
     30
-    (0, testPlants)
-    draw
+    (UIState 0 0 testPlantsLg)
+    (pure . draw)
     handleInput
-    step
+    step 
--- src/Plant.hs
+++ src/Plant.hs
@@ -21,3 +21,20 @@ testPlants =
         Plant "Bouteloua Dactyloides" "Buffalograss" "Full Sun" "Very Low" "Summer"
     ]
 
+testPlantsLg :: [Plant]
+testPlantsLg = 
+    [
+        Plant "Blue Flax" "Linum lewisii" "Full Sun" "Low" "Spring to Summer", 
+        Plant "Chocolate Flower" "Berlandiera lyrata" "Full Sun" "Low" "Summer",
+        Plant "Buffalograss" "Bouteloua dactyloides" "Sun" "Low" "Summer",
+        Plant "Purple Prairie Clover" "Dalea purpurea" "Sun" "Low" "Summer",
+        Plant "Rocky Mountain Penstemon" "Penstemon strictus" "Sun" "Low" "Late Spring" ,
+        Plant "Golden Columbine" "Aquilegia chrysantha" "Part Shade" "Medium" "Spring",
+        Plant "Maximilian Sunflower" "Helianthus maximiliani" "Full Sun" "Medium" "Fall",
+        Plant "Desert Marigold" "Baileya multiradiata" "Full Sun" "Low" "Spring to Fall",
+        Plant "Indian Blanket" "Gaillardia pulchella" "Full Sun" "Low" "Summer",
+        Plant "Firewheel" "Gaillardia aristata" "Full Sun" "Medium" "Late Spring to Fall",
+        Plant "Little Bluestem" "Schizachyrium scoparium" "Sun" "Low" "Late Summer",
+        Plant "Apache Plume" "Fallugia paradoxa" "Full Sun" "Low" "Spring to Fall"
+    ]
+
